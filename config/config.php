718df86c07045adfb9912d6b95cf86afb1c4ff1b
Rmadusanka <roshanmw91@gmail.com>
2019-02-25 23:25:50 +0530

    login function

--- config/config.php
+++ config/config.php
@@ -1,3 +1,6 @@
-<?php
-    
-?>
+<?php 
+$conn = mysqli_connect("localhost", "root", "", "attendee_rp");
+if (!$conn) {
+    echo "connection failed" . mysqli_connect_error();
+}
+ 
--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
-    
+    require "config/config.php";
+
+    $error = "";
+
+    if(isset($_POST['submit'])){ 
+        $username = "";
+        $password = "";
+
+        if (isset($_POST['username']) && $_POST['password']) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+        } else {
+            $error = 'Something went wrong';
+        }
+
+        if ($username && $password) {
+
+            $sql = "SELECT * FROM login WHERE username='".$username."' AND password='".md5($password)."'";
+            $query = mysqli_query($conn,$sql) or die("There is an login error");
+            $res = mysqli_fetch_array($query);
+            if(!empty($res['username']) && !empty($res['password'])){
+                session_start();
+                $_SESSION['username']=$mail;
+                header('location:main.php');
+            }else{
+                $error = "Invaild username or password";
+            }
+        } else {
+            $error = "Please Enter Username and Password";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +42,14 @@
 </head>
 <body>
     <div>
-        login
+        <div>
+            <?php echo $error; ?>
+        </div>
+        <form action="index.php" method="post">
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <input type="submit" name="submit">
+        </form>
     </div>
 </body>
 </html>
